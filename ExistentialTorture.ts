3f072a99b279fe62baa6703b34553719a808573d
PatteeSolomon <patteesolomon@example.com>
2025-03-09 19:42:21 -0500

    Im doing the thing and its cool. I have a vision. Hopefully it isnt already done and I dont take too long with this. Also so sorry for the lack of apostraphies and grammar. I need to know how to make that in a comment.

--- ExistentialTorture.ts
+++ ExistentialTorture.ts
@@ -1,7 +1,6 @@
 import {Stack} from './Stacks';
 import { dfs } from './DFS';
 import {Heap} from './Heap';
-// import {Tree} from './Tree'
 // Anarchy is a node  
 // this is a rootNode
 
@@ -16,7 +15,14 @@ export interface AutoCalls
 /* Anarchy tree class */
 
 export class Anarchy<T>{
+
+    // this is a set of actions on its own.
+    // a tree of routines to be set and executed.
     
+    // on the list of actions to complete (_agenda method calls)
+    // its the priority number.
+    // TODO: this doesn't reflect the level of pertinence 
+    // up on execution just yet
     private _priority: number;
     public get priority(): number {
         return this._priority;
@@ -24,6 +30,7 @@ export class Anarchy<T>{
     public set priority(value: number) {
         this._priority = value;
     }
+    /*THis is the item for the list of actions wthin core tasks */
     private _agenda: void;
     public get agenda(): void {
         return this._agenda;
@@ -54,13 +61,19 @@ export class Anarchy<T>{
         this.rightNode = s;
     }
 
-    task(item: T): void {
-        throw new Error("Method not implemented.");
+    //storing a method.
+    // of void type only //
+    task(item: Function): void {
+        console.log("Method called");
+        item();
     }
 
     // whatever methods
     public doIt(): void {
         console.log("I'm doing it!");
+        /*
+            methods obviously go here
+        */
     }
 }
 
@@ -69,8 +82,8 @@ export class Yddrasil {
     private _:Anarchy<Node>; // root tree node
     private coreTasks:Stack<Anarchy<Node>>; // First in last out
     private heap:Heap // anarchy nodes go here (a tree)
-    private completions:Heap // completed tasks's nodes go here 
-    private node_count:number;  
+    private completions:Heap // completed tasks's nodes go here (i need this for objectives for debugging)
+    private node_count:number;  // how many nodes exist in the forest.
     private _nodeBank:Node; // linked List in the tree (one of many)
 
     public get nodeBank():Node {
@@ -109,7 +122,6 @@ export class Yddrasil {
     public AppendAtEnd(nodeIn:Node):void{
         this._nodeBank.lastChild?.appendChild(nodeIn);
     }
-
     // AppendBeginning and saving the last value
     public AppendBeginning(nodeIn:Node):void{
         let newe = new Node();
@@ -117,7 +129,7 @@ export class Yddrasil {
         newe.appendChild(this._nodeBank);
         this._nodeBank = newe;
     }
-
+    //TODO: find better a way to do this if possible
     public Preorder(node:Anarchy<Node>){
         if(node === null){
             return;
@@ -129,7 +141,7 @@ export class Yddrasil {
         this.Preorder(node.LeftNode)
         this.Preorder(node.RightNode)
     }
-
+    //TODO: find better a way to do this if possible
     // Function to print postorder traversal
     public Postorder(node:Anarchy<Node>) {
         if(node == null){
@@ -143,7 +155,7 @@ export class Yddrasil {
         // method here
         this.Process(node);
     }
-
+    //TODO: find better a way to do this if possible
     public InProcessTraversal(node:Anarchy<Node>){
 
         // Base case
@@ -158,7 +170,7 @@ export class Yddrasil {
 
         // Recur on the right subtree
         this.InProcessTraversal(node.RightNode);
-    }
+    } 
 
     public Process(node:Anarchy<Node>):void {
         let e = node.LeftNode;
@@ -233,6 +245,7 @@ export class Yddrasil {
     // init node calls | init Nodes first with continue?
 
     public init():void { 
+
     }
 
     // condition == ie: I need all my graphics rendered that's it..
@@ -319,7 +332,24 @@ export class Yddrasil {
     /**.
      * my vision of this project
      * is to make 
-     * 
+     * an algorithm that is friendly 
+     * to beginners and ai users 
+     * that allows for ease of implementation
+     * ease of use and debugging going forward
+     * in development.
+     * All without the restraints of any limiting
+     * design pattern already used in SWE or 
+     * gamedev in a chaotic format with more 
+     * overhead command. 
+     * I use Norse mythology because I want the 
+     * commands that control this software you use
+     * this for more or less to be your 'magic'
+     * that controls this rather than try/catch
+     * or something generic.
+     * A closely node based Data Structure with
+     * more dynamic power in just a patter that is
+     * easy to understand with ai implementation
+     * compatibility.
      */
 } 
  
